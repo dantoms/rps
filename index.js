a2b4a0ff559f3356552f29c47a4a6c7da59417e4
0garrow <0garrow@protonmail.com>
2023-03-15 03:06:06 +0000

    add function to get and format user input for chosen object

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function getComputerChoice() {
     if (randomNumber === 0) {
         computerChoice = "ROCK";
     } 
-    
     else if (randomNumber === 1) {
         computerChoice = "PAPER";
     }
@@ -13,9 +12,30 @@ function getComputerChoice() {
         computerChoice = "SCISSORS";
     }
 
-    return computerChoice
+    return computerChoice;
     
 }
+
+function getPlayerChoice() {
+    let playerChoice
+    while (playerChoice !== "r" && playerChoice !== "s" && playerChoice !== "p") {
+        console.log(playerChoice)
+        playerChoice = prompt("Choose your weapon, warrior!\n\nRock, Paper or Scissors? ", "r");
+    }
+    
+    if (playerChoice === "r") {
+        playerChoice = "ROCK"
+    }
+    else if (playerChoice === "p") {
+        playerChoice = "PAPER"
+    }
+    else {
+        playerChoice = "SCISSORS"
+    }
+
+    return playerChoice;
+}
+
 let computerChoice = getComputerChoice()
-let playerChoice = getPlayerChoice
+let playerChoice = getPlayerChoice()
 console.log("Computer: " + computerChoice + ". Player: " + playerChoice)
