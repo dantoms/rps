07aeb5b1b0665b18fd87173c1a78e2c3cd7e20d7
0garrow <0garrow@protonmail.com>
2023-04-10 21:45:03 +0100

    Reformat to allow GUI

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
+const choices = document.querySelectorAll('.choices');
+const scoresElement = document.querySelector('#scores');
+scoresElement.innerText = "Let's play RPSFU!";
+
+choices.forEach((choice) => {
+        choice.addEventListener('click', (event) => {
+                game(event.target.innerText);
+        });
+});
+
 function getComputerChoice() {
-  let computerChoice;
-  let randomNumber = Math.floor(Math.random() * 3);
+        let computerChoice;
+        let randomNumber = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 0) {
-    computerChoice = 'ROCK';
-  } else if (randomNumber === 1) {
-    computerChoice = 'PAPER';
-  } else {
-    computerChoice = 'SCISSORS';
-  }
+        if (randomNumber === 0) {
+                computerChoice = 'ROCK';
+        } else if (randomNumber === 1) {
+                computerChoice = 'PAPER';
+        } else {
+                computerChoice = 'SCISSORS';
+        }
 
-  return computerChoice;
+        return computerChoice;
 }
 
 function playRound(playerChoice, computerChoice) {
-  //getComputerChoice();
+        //getComputerChoice();
 
-  // Create set
-  let winningPairs = new Set();
-  // Add elements to the set
-  winningPairs.add('ROCKSCISSORS');
-  winningPairs.add('PAPERROCK');
-  winningPairs.add('SCISSORSPAPER');
+        // Create set
+        let winningPairs = new Set();
+        // Add elements to the set
+        winningPairs.add('ROCKSCISSORS');
+        winningPairs.add('PAPERROCK');
+        winningPairs.add('SCISSORSPAPER');
 
-  if (computerChoice === playerChoice.toUpperCase()) {
-    return "It's a draw. You both fuckin' stink!";
-  } else if (winningPairs.has(playerChoice.toUpperCase() + computerChoice)) {
-    playerScore++;
-    return "You win. Whoop de fuckin' do. Is your mother proud of you?";
-  } else {
-    computerScore++;
-    return 'LOSER! Hey everyone, come and see how much of a loser this guy is!';
-  }
+        if (computerChoice === playerChoice.toUpperCase()) {
+                return;
+        } else if (
+                winningPairs.has(playerChoice.toUpperCase() + computerChoice)
+        ) {
+                playerScore++;
+                return;
+        } else {
+                computerScore++;
+                return;
+        }
 }
 
-function game() {
-  //const playerChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
-  const computerChoice = getComputerChoice();
-  console.log(playRound(playerChoice, computerChoice));
-  console.log(`Game ${i + 1}\n
-    YOU:${playerChoice} === COMPUTER:${computerChoice}\n
-    Scores \nPlayer:${playerScore} - Computer:${computerScore}`);
+function game(playerChoice) {
+        const computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+        scoresElement.innerText = `🫵 - ${playerChoice}\n🖥️ - ${computerChoice}\n
+        Scores \nPlayer:${playerScore} - Computer:${computerScore}`;
 
-  console.log(`Final Scores\n
-    Player:${playerScore} - Computer:${computerScore}`);
+        if (playerScore == 5 || computerScore == 5) {
+                let winner;
+                if (playerScore == 5) {
+                        winner =
+                                "You win. Whoop de fuckin' do. Is your mother proud of you?";
+                } else {
+                        winner =
+                                'LOSER! Hey everyone, come and see how much of a loser this guy is!';
+                }
+                scoresElement.innerText = `Final Scores\nPlayer:${playerScore} - Computer:${computerScore}\n${winner}`;
+
+                playerScore = 0;
+                computerScore = 0;
+        }
 }
 
 let playerScore = 0;
 let computerScore = 0;
-game();
+//game();
