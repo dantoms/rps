09774bb56c1b97b42136cb5a03a959b49c4ffc6e
0garrow <0garrow@protonmail.com>
2023-04-10 05:41:41 +0100

    Properly indent game function

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
 function getComputerChoice() {
-    let computerChoice
-    let randomNumber = Math.floor(Math.random() * 3);
+  let computerChoice;
+  let randomNumber = Math.floor(Math.random() * 3);
 
-    if (randomNumber === 0) {
-        computerChoice = "ROCK";
-    } 
-    else if (randomNumber === 1) {
-        computerChoice = "PAPER";
-    }
-    else {
-        computerChoice = "SCISSORS";
-    }
+  if (randomNumber === 0) {
+    computerChoice = 'ROCK';
+  } else if (randomNumber === 1) {
+    computerChoice = 'PAPER';
+  } else {
+    computerChoice = 'SCISSORS';
+  }
 
-    return computerChoice;
-    
+  return computerChoice;
 }
 
 function playRound(playerChoice, computerChoice) {
+  //getComputerChoice();
 
-    //getComputerChoice();
-    
-    // Create set
-    let winningPairs = new Set();
-    // Add elements to the set
-    winningPairs.add("ROCKSCISSORS");
-    winningPairs.add("PAPERROCK");
-    winningPairs.add("SCISSORSPAPER");
+  // Create set
+  let winningPairs = new Set();
+  // Add elements to the set
+  winningPairs.add('ROCKSCISSORS');
+  winningPairs.add('PAPERROCK');
+  winningPairs.add('SCISSORSPAPER');
 
-
-    if (computerChoice === playerChoice.toUpperCase()) {
-        return "It's a draw. You both fuckin' stink!";
-    }
-    else if (winningPairs.has(playerChoice.toUpperCase() + computerChoice)) {
-        playerScore++;
-        return "You win. Whoop de fuckin' do. Is your mother proud of you?";
-    }
-    else {
-        computerScore++;
-        return "LOSER! Hey everyone, come and see how much of a loser this guy is!";
-    }
-
+  if (computerChoice === playerChoice.toUpperCase()) {
+    return "It's a draw. You both fuckin' stink!";
+  } else if (winningPairs.has(playerChoice.toUpperCase() + computerChoice)) {
+    playerScore++;
+    return "You win. Whoop de fuckin' do. Is your mother proud of you?";
+  } else {
+    computerScore++;
+    return 'LOSER! Hey everyone, come and see how much of a loser this guy is!';
+  }
 }
 
 function game() {
-        const playerChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
-        const computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice))
-        console.log(`Game ${i+1}\n
-        YOU:${playerChoice} === COMPUTER:${computerChoice}\n
-        Scores \nPlayer:${playerScore} - Computer:${computerScore}`);
-    }
+  const playerChoice = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
+  const computerChoice = getComputerChoice();
+  console.log(playRound(playerChoice, computerChoice));
+  console.log(`Game ${i + 1}\n
+    YOU:${playerChoice} === COMPUTER:${computerChoice}\n
+    Scores \nPlayer:${playerScore} - Computer:${computerScore}`);
 
-    console.log(`Final Scores\n
+  console.log(`Final Scores\n
     Player:${playerScore} - Computer:${computerScore}`);
 }
 
